1ce3fe99a3503be8afe98122f079f745b75ab509
Vera Radaeva <pobratimlok@gmail.com>
2021-03-08 18:39:27 +0300

    Adding drawing

--- cars/src/GameIO.hs
+++ cars/src/GameIO.hs
@@ -1,8 +1,63 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module GameIO
     ( runGame
     ) where
 
 import Types
+import Draw (drawRoad, drawSettings)
+
+import Graphics.Gloss.Interface.IO.Game
+import Graphics.Gloss.Data.Bitmap
+import System.IO
+import System.Random
+import Data.List
+
+------------------
+--Отриcовка-------
+------------------
+
+display :: Display
+display = InWindow "cars" (500, 500) (-500, -500)
+
+bgColor :: Color
+bgColor = white
+
+drawApp :: Experiment -> IO Picture
+drawApp Experiment{..}
+  | (appState == Settings) = return $ drawSettings
+  | otherwise = return $ drawRoad road
+
+--------------------
+--Нажатие клавиш----
+--------------------
+
+handleEvent :: Event -> Experiment -> IO Experiment
+handleEvent _ state = return state
+
+-----------------------------------------------------------------
+--Обработка состояния (проиcходящее без нажатия клавиш)----------
+-----------------------------------------------------------------
+
+updateApp :: Float -> Experiment -> IO Experiment
+updateApp _ state = return state
+
+--------------------------------------------------------
+
+fps :: Int
+fps = 1
 
 runGame :: IO ()
-runGame = putStrLn "someFunc"
+runGame = do
+  gen <- getStdGen
+  let
+    car1 = Car 1 1 100 Acceleration
+    car2 = Car 1 1 600 Braking
+    car3 = Car 1 1 400 ConstantSpeed
+    car4 = Car 1 1 800 Accident
+    roadLane1 = RoadLane 1 10 [100] [car1, car2]
+    roadLane2 = RoadLane 0 0 [900] [car3]
+    roadLane3 = RoadLane (-1) 200 [200] [car4]
+    initRoad = Road 5 [roadLane1, roadLane2, roadLane3]
+    initState = Experiment Motorway (40, 70) (3, 5) 10 5 initRoad
+  playIO display bgColor fps initState drawApp handleEvent updateApp
--- cars/src/Types.hs
+++ cars/src/Types.hs
@@ -1,29 +1,34 @@
 module Types where
 
 data Experiment =  Experiment
-  { n :: Int -- количестов полос
+  { appState :: AppState
   , rangeV :: (Int, Int) -- диапазон скоростей автомобилей
   , rangeT :: (Int, Int) -- интервал между появлениями автомобилей на дороге
   , deltaV :: Int -- величина уменьшения скорости искуственно приторм машины
   , deltaT :: Int -- время движения искуственно притормаживаемой машины с меньшей скоростью
+  , road :: Road
   }
 
-data Road = Road 
-  { accidentT :: Int -- время аварии (длительность) 
+data Road = Road
+  { accidentT :: Int -- время аварии (длительность)
+  , roadLane :: [RoadLane]
   }
 
 data RoadLane = RoadLane
-  { number :: Int -- номер полосы
+  { number :: Float -- номер полосы [-2, 1]
   , frequencyСar :: Int -- частота появления нового автомобиля
-  , slowdown :: Int -- координата замедления
+  , slowdown :: [Float] -- координаты замедления
+  , car :: [Car]
   }
 
 data Car = Car
   { v0 :: Int -- начальная скорость
   , v :: Int -- текущая скоросоть
-  , x :: Int -- текущая координата
+  , x :: Float -- текущая координата
   , status :: StatusCar -- состояние машины
   }
 
-data StatusCar = Acceleration | Braking | ConstantSpeed | Accident
+data StatusCar = Acceleration | Braking | ConstantSpeed | Accident deriving (Eq)
 -- Ускорение | Торможение | Постоянная скорость | Авария
+
+data AppState = Settings | Motorway deriving (Eq)
